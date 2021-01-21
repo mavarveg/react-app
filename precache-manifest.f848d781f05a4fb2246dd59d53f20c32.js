afcb02d2f18219b5c4aaa250defa95c9312b3851
mavarveg <mavarg07@gmail.com>
2021-01-21 15:16:45 -0600

    Updates

---
+++ precache-manifest.f848d781f05a4fb2246dd59d53f20c32.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ad298c116d68e8384581e54d6d2cbe",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "59b5a6158d64b4166f71",
+    "url": "/react-app/static/css/main.066bfdef.chunk.css"
+  },
+  {
+    "revision": "48257b4f655a777ae4aa",
+    "url": "/react-app/static/js/2.1cac8828.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/react-app/static/js/2.1cac8828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b5a6158d64b4166f71",
+    "url": "/react-app/static/js/main.b9724bf0.chunk.js"
+  },
+  {
+    "revision": "116dcb4fca339999d655",
+    "url": "/react-app/static/js/runtime-main.0f6ec470.js"
+  },
+  {
+    "revision": "226c30172a0b3d43e93c047dd0cbcdb0",
+    "url": "/react-app/static/media/map.226c3017.png"
+  },
+  {
+    "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
+    "url": "/react-app/static/media/profile_photo.c7b5f6a0.jpg"
+  },
+  {
+    "revision": "e1613e3e5beacafffe2dd9e8037dfc44",
+    "url": "/react-app/static/media/project1.e1613e3e.png"
+  },
+  {
+    "revision": "b2f649e7e652a24067ea8f960bdd80cf",
+    "url": "/react-app/static/media/project2.b2f649e7.png"
+  },
+  {
+    "revision": "a3d72ccd0ae21a28d7ccc7e816ab30bc",
+    "url": "/react-app/static/media/project3.a3d72ccd.png"
+  },
+  {
+    "revision": "e78d26131b30631ab42b8ebbdcce82cc",
+    "url": "/react-app/static/media/project4.e78d2613.png"
+  }
+]);
