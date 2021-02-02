a9decaba1cc8242ebf41c17032c5db5e6079dfa5
mavarveg <mavarg07@gmail.com>
2021-02-02 09:15:49 -0600

    Ajustes de about

--- src/contents/About.js
+++ src/contents/About.js
@@ -7,16 +7,12 @@ class About extends Component {
             <div className="condiv">
             <h1 className="subtopic">About Me</h1>
             <div className="hr_line line2 glass"><div className="inner2"></div></div>
-            <h2 className="abouthead">Vikalp <mark className="mark2">Kaushik</mark></h2>
+            <h2 className="abouthead">Manuel<mark className="mark2">VArgas Vega</mark></h2>
             <p>Full Stack Developer</p>
             <br></br>
-            <p className="p10">I started my journey in the world of computers from an young age,
-            now I’m 21 years old, Pursuing my Computer Science and Engineering Degree in
-            GNDEC Ludhiana.  Web development is my center of interest, i always
-            love the idea of cross-platform development, 1-n one code base deploy into almost
-            any platform, which web technology like Javascript enables me to do. I also like
-            creating Interactive UI components for better UX  and share those design and codes
-            to the world through Github and Instagram.
+            <p className="p10">Systems Engineer with 9+ years of experience in software development 
+            with high levels of productivity, scalability, and maintainability. Good skills in agile practices, 
+            Scrum, Empathy, Collective properties, Initiative to hangout with the team, Research, proactive and a team player.
             </p>
             <div className="Edu">
                 <div class="back1 back2"><i class="fa fa-book i1 i3"></i></div>
@@ -26,20 +22,20 @@ class About extends Component {
                 <div className="vr_inner inn1"></div>
                 <div className="vr_inner inn2"></div>
             </div>
-            <h4>B.Tech in CSE</h4>
-            <p className="p1">GNDEC, Ludhiana</p>
-            <p className="p1"><i class="fa fa-calendar i2"></i>May 2018 - 2022</p>
-            <h4>High School</h4>
-            <p className="p1">St. Farid Public School, MGG</p>
-            <p className="p1"><i class="fa fa-calendar i2"></i>May 2017</p>
+            <h4>UNED</h4>
+            <p className="p1">Degree Web Application Development</p>
+            <p className="p1"><i class="fa fa-calendar i2"></i>2016 - 2018</p>
+            <h4>UCR</h4>
+            <p className="p1"> Bachellor Degree in Bussiness Computer’s Science</p>
+            <p className="p1"><i class="fa fa-calendar i2"></i>2008 - 2011</p>
 
             <h3 class="sk_head">Skills</h3>
-            <div class="sk glass">HTML</div>
-            <div class="sk s2 glass">CSS</div>
             <div class="sk s3 glass">JavaScript</div>
-            <div class="sk s4 glass">React</div>
-            <div class="sk s5 glass">Node</div>
-            <div class="sk s6 glass">Python</div>
+            <div class="sk s4 glass">ReactJs</div>
+            <div class="sk s5 glass">NodeJs</div>
+            <div class="sk s6 glass">Scrum</div>
+            <div class="sk glass">Typescript</div>
+            <div class="sk s2 glass">Docker</div>
             </div>
             )
         }
