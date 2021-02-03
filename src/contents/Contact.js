95fcab282beb7fe7534ad02214cb3f77722d2690
mavarveg <mavarg07@gmail.com>
2021-02-02 23:44:24 -0600

    ajustes para portafolio

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -8,9 +8,8 @@ class Contact extends Component {
             <div className="condiv contact">
             <h1 className="subtopic">Contact Me</h1>
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
-            <div class="con_out glass"><img src={Map} class="co1"></img></div>
             <a href="#"><div className="back3 glass">Download Resume</div></a>
-            <a href="mailto:youremail@gmail.com"><div className="back3 back4 glass">Contact Me</div></a>
+            <a href="mailto:manuel.vargas.vega@outlook.com"><div className="back3 back4 glass">Contact Me</div></a>
             <div className="hr_line line3 glass"><div className="inner3"></div></div>
             <Social />
             </div>
--- src/contents/Portfolio.js
+++ src/contents/Portfolio.js
@@ -10,10 +10,10 @@ class Portfolio extends Component {
             <div className="condiv">
             <h1 className="subtopic">My Work</h1>
             <div className="hr_line line2 glass"><div className="inner2 inner3"></div></div>
-            <div class="port_out port1 glass"><a href="https://github.com/vikalp2502/AlgoSort-Sorting-Visualiser"><img src={project1} className="po1"></img></a></div>
-            <div class="port_out port2 glass"><a href="https://github.com/vikalp2502/Shoes-Nike-Landing-page"><img src={project2} className="po1"></img></a></div>
+            <div class="port_out port1 glass"><a href="http://ascatur.com"><img src={project1} className="po1"></img></a></div>
+            <div class="port_out port2 glass"><a href="http://administrativo.ascatur.com/Account/Login"><img src={project2} className="po1"></img></a></div>
             <div class="port_out port3 glass"><a href="https://github.com/vikalp2502/To-do-list-v2"><img src={project3} className="po1"></img></a></div>
-            <div class="port_out port4 glass"><a href="https://github.com/vikalp2502/Tribute-to-msd"><img src={project4} className="po1"></img></a></div>
+            <div class="port_out port4 glass"><a href="https://mavarveg.github.io/react-gifexpertapp/"><img src={project4} className="po1"></img></a></div>
             </div>
             )
         }
