b848d30bc1eedd41e7dbaadcc8db5e44fb882cdc
mavarveg <mavarg07@gmail.com>
2021-02-02 23:46:46 -0600

    Updates

---
+++ precache-manifest.c0565eba5426a0d9fc4317495671bcfb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a9a30bd7601c91da009374a06f1306",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c9f3f17d61b1fca7553d",
+    "url": "/react-app/static/css/main.b3ed7e9a.chunk.css"
+  },
+  {
+    "revision": "cb47ba21cdad840b4423",
+    "url": "/react-app/static/js/2.37a3cd8d.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/react-app/static/js/2.37a3cd8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9f3f17d61b1fca7553d",
+    "url": "/react-app/static/js/main.a8c3e8af.chunk.js"
+  },
+  {
+    "revision": "116dcb4fca339999d655",
+    "url": "/react-app/static/js/runtime-main.0f6ec470.js"
+  },
+  {
+    "revision": "226c30172a0b3d43e93c047dd0cbcdb0",
+    "url": "/react-app/static/media/map.226c3017.png"
+  },
+  {
+    "revision": "f24ecba57741b65f6b67d099c52132b8",
+    "url": "/react-app/static/media/profile_photo.f24ecba5.jpg"
+  },
+  {
+    "revision": "aeac7d334804a34db0787cd0211ca5f2",
+    "url": "/react-app/static/media/project1.aeac7d33.png"
+  },
+  {
+    "revision": "030cc28416b1d0bce85b9d6e0f528141",
+    "url": "/react-app/static/media/project2.030cc284.png"
+  },
+  {
+    "revision": "a3d72ccd0ae21a28d7ccc7e816ab30bc",
+    "url": "/react-app/static/media/project3.a3d72ccd.png"
+  },
+  {
+    "revision": "9500e0cd9f555f6f7527c75c1e082155",
+    "url": "/react-app/static/media/project4.9500e0cd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.914d892655c3a3db95db282a892f8b33.js"
+  "/react-app/precache-manifest.c0565eba5426a0d9fc4317495671bcfb.js"
 );
 
 self.addEventListener('message', (event) => {
