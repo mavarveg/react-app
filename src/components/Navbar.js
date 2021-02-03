e2fe12045f52944666d92dfb3a4d2c82702622db
mavarveg <mavarg07@gmail.com>
2021-02-03 00:01:46 -0600

    cambio de profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,7 +22,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="glass">
-            <img src={profilepic} className="profilepic"></img>
+            <img src={profilepic}></img>
             <h2>Manuel <mark>Vargas Vega</mark></h2>
             <p>Full Stack Developer</p>
             <img align="center" alt="HTML5" width="26px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
